468d4456058bd0c8940fb3059c31932fff2f8d48
Alan-Aceves <98369880+Alan-Aceves@users.noreply.github.com>
2022-05-25 12:52:12 -0600

    Update MicTest_3.cpp

--- MicTest/MicTest_3.cpp
+++ MicTest/MicTest_3.cpp
@@ -15,24 +15,24 @@ using namespace std;
 #define left  32
 #define front 35
 
-arduinoFFT FFT = arduinoFFT();
+arduinoFFT FFT = arduinoFFT(); //initializing FFT
 
 int i ;
 
-const int SAMPLE = 1024;
-const int tolerance= 2600;
+const int SAMPLE = 1024;    // Sampling period 128 bytes = 1024 bits
+const int tolerance= 2600;  // 1500 to detect low noise :: 2600 to cancel low noise
 
-//Real number
-double vRight [SAMPLE];        
-double vLeft  [SAMPLE];      //use this variables for the microphones comparator  
-double vFront [SAMPLE];
+//// Real number
+double vRight [SAMPLE];      
+double vLeft  [SAMPLE];     // use this variables for the microphones comparator  
+double vFront [SAMPLE];    
 
-//Imaginary number
+//// Imaginary number
 double iRight [SAMPLE];
-double iLeft  [SAMPLE];
+double iLeft  [SAMPLE];    //  only usingned on the Fast Fourier Transform
 double iFront [SAMPLE];
 
-////Data capture in the Array
+//// Data capture in the Array
 void readMicrophones (){
    for (int i = 0 ; i < SAMPLE ; i++){
     vRight[i] = analogRead(right);
@@ -51,39 +51,37 @@ void setup() {
 }
 
 void loop() {
-
- //// Reading Signals
+  //// Reading Signals
   readMicrophones();
-
   ////.1 Fast Fourier Transform Right
   fourierRight();
-  
   ////.1 Fast Fourier Transform Left
   fourierLeft();
-  
   ////.1 Fast Fourier Transform Front
   fourierFront();
-
-  //// SHOWING SPECTRUM DATA
+   
+  //// MASTER COMPARATOR
+  //.3For better performance remove all the couts
   for (i = 0 ; i < 512 ; i++){
-    if ( vRight [i] > tolerance && i > 1 ){ cout << vRight[i] << " Right" << endl;}
-    if ( vLeft  [i] > tolerance && i > 1 ){ cout << vLeft[i]  << " Left"  << endl;}
-    if ( vFront [i] > tolerance && i > 1 ){ cout << vFront[i] << " Front" << endl;}
+    if ( vRight [i] > tolerance && i > 1 ){ cout << vRight[i] << " Right" << endl;}   //.3
+    if ( vLeft  [i] > tolerance && i > 1 ){ cout << vLeft[i]  << " Left"  << endl;}   //.3
+    if ( vFront [i] > tolerance && i > 1 ){ cout << vFront[i] << " Front" << endl;}   //.3
   }
 }
 
+//.1
 void fourierRight(){
   FFT.Windowing           (vRight, SAMPLE, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
   FFT.Compute             (vRight, iRight, SAMPLE, FFT_FORWARD);
   FFT.ComplexToMagnitude  (vRight, iRight, SAMPLE);
 }
-
+//.1
 void fourierLeft(){
   FFT.Windowing           (vLeft, SAMPLE, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
   FFT.Compute             (vLeft, iLeft, SAMPLE, FFT_FORWARD);
   FFT.ComplexToMagnitude  (vLeft, iLeft, SAMPLE);
 }
-
+//.1
 void fourierFront(){
   FFT.Windowing           (vFront, SAMPLE, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
   FFT.Compute             (vFront, iFront, SAMPLE, FFT_FORWARD);
